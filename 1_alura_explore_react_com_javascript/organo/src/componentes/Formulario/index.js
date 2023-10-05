b3882b09585336ec789cde7c2d4b69fd6c04dac7
Andrade <vinicius_andrade2010@hotmail.com>
2023-10-05 17:40:58 -0300

    2/100 - aprendendo react

--- 1_alura_explore_react_com_javascript/organo/src/componentes/Formulario/index.js
+++ 1_alura_explore_react_com_javascript/organo/src/componentes/Formulario/index.js
@@ -2,6 +2,7 @@ import './Formulario.css';
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import {useState} from "react";
 
 const Formulario = () => {
 
@@ -14,19 +15,55 @@ const Formulario = () => {
         'Mobile'
     ];
 
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('');
+
     const aoSalvar = (evento) => {
         evento.preventDefault();
-        console.log('Salvando')
+        console.log('Formulario foi submetido =>', nome, cargo, imagem,time);
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2> Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <ListaSuspensa itens={times} label="Time"/>
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => {
+                        setNome(valor)
+                    }}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => {
+                        setCargo(valor)
+                    }}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={
+                        valor => setImagem(valor)
+                    }
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time"
+                    itens={times}
+                    valor={time}
+                    aoAlterado={
+                        valor => setTime(valor)
+                    }
+                />
                 <Botao>Criar Card</Botao>
             </form>
         </section>
--- 1_alura_explore_react_com_javascript/organo/src/componentes/ListaSuspensa/index.js
+++ 1_alura_explore_react_com_javascript/organo/src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,7 @@ const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa' >
             <label>{props.label}</label>
-            <select required={props.require} >
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.require} value={props.value}>
                 {props.itens.map((item, index) => <option key={index}>{item}</option>)}
             </select>
         </div>
